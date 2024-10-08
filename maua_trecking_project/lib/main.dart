01626891de563a398c8d546a53d3b7215fe4ced2
Uper05 <jvfdessani@gmail.com>
2024-10-08 12:45:57 -0300

    Tela de Registro
    Implementação da tela de registro de usuário Mobile, possuindo um título, 5 campos para inserir RA e uma função de salvamento em lista.
    
    ainda falta implementar:
    -Logo da Instituição
    -Troca de tela após registro
    -Tela de Professores

--- maua_trecking_project/lib/main.dart
+++ maua_trecking_project/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'widgets/ralist.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,116 +11,121 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Maua Trekking',
+      home: Home(title: 'Maua Trekking'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class Home extends StatelessWidget {
+  const Home({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      body: LayoutBuilder(builder: (context, constrains){
+        return constrains.maxWidth <= 900?const MobileLayout():const DesktopLayout();
+      })
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class MobileLayout extends StatefulWidget{
+  const MobileLayout({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return MobileLayoutState();
+  }
+}
+
+
+class MobileLayoutState extends State<MobileLayout>{
+
+  final List<String> ratitulos = [
+    'RA Líder',
+    'RA Companheiro 1',
+    'RA Companheiro 2',
+    'RA Companheiro 3',
+    'RA Companheiro 4'
+  ];
+
+  final List<IconData> raicones = [
+    Icons.person,
+    Icons.group, 
+    Icons.group,
+    Icons.group,
+    Icons.group
+  ];
+ 
+  late List<TextEditingController> controllers;
+
+  @override
+  void initState() {
+    super.initState();
+    controllers = List.generate(ratitulos.length, (index) => TextEditingController());
+  }
+
+  void getTextFieldValues() {
+    List<String> valores = controllers.map((controller) => controller.text).toList();
+    print(valores);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold( 
+      backgroundColor: const Color.fromARGB(255, 0, 71, 133),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const Text(
+            "Maua Trekking",
+            style: TextStyle(
+                fontSize: 24, color: Color.fromARGB(255, 223, 223, 223)),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: screenWidth * 0.75,
+            height: screenHeight * 0.60,
+            child: Container(
+              decoration: BoxDecoration(
+                  color:const Color.fromARGB(255, 223, 223, 223),
+                  borderRadius: BorderRadius.circular(15)),
+              child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ListView
+                    ( 
+                      children: [
+                      RaListWidget(icon: raicones[0], title: ratitulos[0], controller: controllers[0],),
+                      RaListWidget(icon: raicones[1], title: ratitulos[1], controller: controllers[1]),
+                      RaListWidget(icon: raicones[2], title: ratitulos[2], controller: controllers[2]),
+                      RaListWidget(icon: raicones[3], title: ratitulos[3], controller: controllers[3]),
+                      RaListWidget(icon: raicones[4], title: ratitulos[4], controller: controllers[4]),
+                    ],)
+                  )),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          const SizedBox(height: 20,),
+          ElevatedButton(onPressed: getTextFieldValues,child: const Icon(Icons.arrow_forward,color: Color.fromARGB(255, 0, 71, 133),),)
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class DesktopLayout extends StatelessWidget{
+  const DesktopLayout({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
---
+++ maua_trecking_project/lib/widgets/ralist.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class RaListWidget extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final TextEditingController controller; // Adiciona o controlador como um parâmetro
+
+
+  const RaListWidget({super.key, required this.icon, required this.title, required this.controller});
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Icon(icon,color: Color.fromARGB(255, 0, 71, 133),),
+            const SizedBox(width: 10),
+            Text(
+              title,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold,color: Color.fromARGB(255, 0, 71, 133),),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          controller: controller, // Associando o controlador ao TextField
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText:'Digite o RA',
+          ),
+        ),
+        const SizedBox(height: 20), // Espaço entre os widgets
+      ],
+    );
+  }
+}
+
