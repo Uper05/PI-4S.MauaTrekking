932ba7e25ba475c181488d595037e785f1ba7f85
Tamie <125300145+T4mie@users.noreply.github.com>
2024-10-23 19:44:29 -0300

    tirando o database antigo

--- maua_trecking_project/lib/main.dart
+++ maua_trecking_project/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:maua_trecking_project/src/app.dart';
 
 //imports do banco de dados
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:maua_trecking_project/src/screens/teacherscreen.dart';
 import 'firebase_options.dart';
 
